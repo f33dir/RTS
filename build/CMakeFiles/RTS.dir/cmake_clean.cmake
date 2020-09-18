81e74ec3a96adaef555c26ce2d4581a0165801c9
f33dir <arseniy.tarasov@gmail.com>
2020-09-18 11:42:56 +0300

    idk but now it seems to work

---
+++ build/CMakeFiles/RTS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RTS.dir/main.cpp.o"
+  "RTS"
+  "RTS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RTS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/f33dir/projects/GODOT/RTS
+# Build directory: /home/f33dir/projects/GODOT/RTS/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
