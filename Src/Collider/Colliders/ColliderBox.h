41567357606fe8d4029308c153c666b5541e3887
f33dir <arseniy.tarasov@gmail.com>
2020-09-22 12:19:52 +0300

    reset

--- Src/Collider/Colliders/ColliderBox.h
+++
@@ -1,8 +0,0 @@
-#ifndef COLLIDERBOX_H
-#define COLLIDERBOX_H
-#include "../Collider.h"
-class ColliderBox : public Collider{
-public:
-    Collision collide(Collider);
-};
-#endif // !COLLIDERBOX_H
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <Ogre.h>
-#include <OgreBites.h>
-#include "Src/Collider/Collider.h"
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
-}
