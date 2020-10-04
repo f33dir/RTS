697b10e90a5c583b28e1d5e11efc29113a7b28be
fenryux <foruseifitneed@yandex.ru>
2020-10-04 17:54:14 +0300

    Object setup

---
+++ Src/Object/TestWorld.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public class TestWorld : Spatial
+{
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
