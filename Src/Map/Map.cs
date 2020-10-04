35609818b3ef7146052b7b3db7a68719d90bb110
f33dir <arseniy.tarasov@gmail.com>
2020-10-04 20:38:27 +0300

    dealing with map

--- Src/Map/Map.cs
+++ Src/Map/Map.cs
@@ -1,13 +1,21 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+
 namespace Map{
     enum TileType
     {
-        Slain,
+        Plain = 0,
         Slope,
         LeftSlope,
         RightSlope,
-        Pit
+        Pit,
+        Edge,
+        EdgeCorner,
+        EdgeInsideCorner,
+        PitEdge,
+        PitEdgeCorner,
+        PitEdgeInsideCorner,
     }
     /*
     .--------X
@@ -15,24 +23,36 @@ namespace Map{
     | . . . .
     | . . . .
     Y
-    
     */
-    class Map
+    class Map : Node
     {
         //fields
-        private MapTile[,] Matrix;
         private int SizeX;
         private int SizeY;
+        private MapTile[][] Matrix;
         //methods
         public void SetTile(MapTile inputTile,int x,int y){
-            Matrix[x,y] = inputTile;
+            Matrix[x][y] = inputTile;
         }
+
         public void PrintMap(){
             for(int i = 0;i<SizeY;i++){
                 for(int j = 0;j<SizeX;j++){
                     GD.Print(Matrix.ToString());
                 }
             }
+        }
+
+        public override void  _Ready(){
+            
+        }
+        public Map(){
+            this.SizeX = 10;
+            this.SizeY = 10;
+        }
+        public Map(int Sizex,int Sizey){
+            this.SizeX = Sizex;
+            this.SizeY = Sizey;
         }
     }
 }
--- Src/Map/MapTile.cs
+++ Src/Map/MapTile.cs
@@ -4,16 +4,16 @@ namespace Map{
     public class MapTile
     {
         private TileType Type{get;set;}
-        public double Rotation;
+        public double Rotation;// 0-3 I suppose
         public int Height;
         public int Color;
 
-        public override string ToString()
-        {
-            return Type.ToString() + " " + Rotation +" " + Height +" " +Color;
-        }
+        // public override string ToString()
+        // {
+        //     return Type.ToString() + " " + Rotation +" " + Height +" " +Color;
+        // }
 
-        MapTile(int type, int rotation, int height, int color)
+        public MapTile(int type, int rotation, int height, int color)
         {
             Type = (TileType)type;
             Rotation = rotation;
@@ -21,13 +21,17 @@ namespace Map{
             Color = color;
         }
         
-        MapTile(String input)
+        public MapTile(String input)
         {
             String[] parameters= input.Split(" ");
             Type = (TileType)int.Parse(parameters[0]);
             Rotation = int.Parse(parameters[1]);
             Height = int.Parse(parameters[2]);
             Color = int.Parse(parameters[3]);
+        }
+        public MapTile()
+        {
+            
         }
     }
 }
